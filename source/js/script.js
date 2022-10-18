fc5d661cca6ea3f5c29127232d0f7535d6861ae1
Eugene <edukalis@gmail.com>
2022-10-18 15:54:59 +0300

    fixed already marker capacity

--- source/js/script.js
+++ source/js/script.js
@@ -4,6 +4,7 @@ const input = document.querySelector('.marker__input');
 const result = document.querySelector('.marker__result');
 const color = document.querySelector('.color');
 const capacityElem = document.querySelector('.capacity');
+const refillBtn = document.querySelector('.refill');
 
 
 class Marker {
@@ -16,25 +17,27 @@ class Marker {
     writeText() {
         input.addEventListener('input', () => {
             result.textContent = input.value;
-            // console.log(`${this.str} ${this.str.length}`);
             return this.str;
         });
     }
 
-    lookStr() {
-        console.log(this.str);
-        console.log(this.str.length);
-    }
-
     changeCapacity() {
-        input.addEventListener('input', () => {
-            this.str = result.textContent.replace(/\s/g, '');
-            let arr = this.str.split('');
-            for(let i = 0; i <= arr.length; i++) {}
-            console.log(arr);
+        input.addEventListener('keydown', (e) => {
+            if (e.which === 32) {
+                // 32 is the CODE OF BUTTON - SPACE
+                this.capacity = this.capacity;
+                console.log(this.capacity);
+            } else {
                 this.capacity -= 0.5;
                 console.log(this.capacity);
-            });
+            }
+
+        });
+    }
+
+    refill() {
+        this.capacity = 100;
+        capacityElem.textContent = `Marker capacity: ${this.capacity}%`;
     }
 
     changeColor() {
@@ -44,21 +47,20 @@ class Marker {
         };
     }
 
-    checkCapacity() {
+    showCapacity() {
         capacityElem.textContent = `Marker capacity: ${this.capacity}%`;
         input.oninput = () => {
             capacityElem.textContent = `Marker capacity: ${this.capacity}%`;
         };
     }
 
-    refillMarker() {
-        this.capacity = 100;
-    }
+
 }
 
 const marker = new Marker('red', 100, 'Hello World');
 
 marker.writeText();
 marker.changeColor();
-marker.checkCapacity();
 marker.changeCapacity();
+marker.showCapacity();
+refillBtn.addEventListener('click', marker.refill());
